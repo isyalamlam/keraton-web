be93b594bab0bc6e0189accf31f0625f7575d45c
Keyzaadn <157870715+Keyzaadn@users.noreply.github.com>
2024-02-29 15:01:52 +0700

    keyza

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
+import CheckoutKeraton from '../views/CheckoutKeraton.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUp
+    },
+    {
+      path: '/checkoutkeraton',
+      name: 'checkoutkeraton',
+      component: CheckoutKeraton
     },
   ]
 })
