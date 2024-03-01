8298e014723d85bcc48ea7f2b03dca3d8a1a46a8
nsumett <aditiaputrariyadi@gmail.com>
2024-03-01 14:09:06 +0700

    Commit local changes to src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
+import AreaKeraton from '../views/AreaKeraton.vue'
+import Museum from '../views/Museum.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +19,17 @@ const router = createRouter({
       name: 'signup',
       component: SignUp
     },
+    {
+      path: '/areakeraton',
+      name: 'areakeraton',
+      component: AreaKeraton
+    },
+    {
+      path: '/museum',
+      name: 'museum',
+      component: Museum
+    },
+ 
   ]
 })
 
