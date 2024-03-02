a0526f25b1d04cbb223efaa2b8388cf755f9bfdb
isyalamlam <isyalamlam@gmail.com>
2024-03-02 18:11:41 +0700

    mencoba pull isya

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,20 @@ export default {
 }
 </script>
 
-
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
 body {
-  font-family: sans-serif;}
-  
-  header{
-    width: 60vw;
-    height: 40vw;
-    background-color: white;
-    box-shadow: 0px 2px 0px rgb(51, 51, 51);
-    padding: 15px;
-    margin-left: 20vh;
-    margin-top: 10vh;
-  }
+  font-family: 'montserrat', sans-serif;
+}
+
+header {
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
+}
 </style>
