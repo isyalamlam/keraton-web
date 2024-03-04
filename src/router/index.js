79572ddf167a070b1f9ebb617a7aa592de0f2b0d
unknown <radithiaannafi77@gmail.com>
2024-03-04 08:20:40 +0700

    Your commit message

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
+import HistoryTransaksi from '../views/HistoryTransaksi.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUp
+    },
+    {
+      path: '/historytransaksi',
+      name: 'historytransaksi',
+      component: HistoryTransaksi
     },
   ]
 })
