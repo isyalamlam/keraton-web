02f059f87266beeb092eae56755b64f11c5bdf2a
Habib Budin <habibudinn@gmail.com>
2024-03-04 10:09:43 +0700

    Commit pesan yang menjelaskan perubahan

---
+++ src/views/SejarahKeraton.vue
@@ -0,0 +1,661 @@
+<template>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <div id="app">
+    <div class="textAll">
+      <div class="navbar">
+      <div class="navbar-left">
+        <img  alt="icon-aplikasi" src="../assets/images/logo_keraton.png" class="app-icon" />
+        <span class="app-name">Keraton Kesepuhan Cirebon</span>
+      </div>
+      <div class="navbar-right">
+        <nav>
+              <ul>
+                  <li><a href="#">Beranda</a></li>
+                  <li><a href="#">Sejarah</a></li>
+                  <li><a href="#">Booking</a></li>
+                  <li><a href="#">Objek Wisata</a></li>
+                  <button @click="getTickets" class="button1">Dapatkan Tiket</button>
+              </ul>
+          </nav>
+        
+        
+      </div>
+    </div>
+      <div class="container">
+       <img src="../assets/images/sejarah.jpg" alt="sejarah" class="fotobkg"> 
+        <div class="upper">
+         {{ uppertext }}
+          </div>
+            <div class="upper-2">
+          {{ uppertext2 }}
+         </div>
+        </div>
+        <div class="info">
+          <div class="info1">
+            <p>Kota Cirebon merupakan kota yang memiliki nilai historis yang sangat kental terutama pada masa
+            kerajaan dan penyebaran agama Islam di Pulau Jawa.
+        
+            <br><br>Salah satu peninggalan pada masa
+            kerajaan tersebut adalah sebuah bangunan yang hingga kini masih berdiri dengan kokoh di kota
+            ini yaitu Rumah Adat Kasepuhan Cirebon.</p>
+            <img src="../assets/images/macanalikasepuhan.png" alt="">
+          </div>
+         <div class="info2">
+          <img src="../assets/images/pendopo.png" alt="">
+          <div class="info2-text">
+          
+            .
+          <p class="between">Pada abad XV (-+ 1430)
+            
+            <br><br>Pangeran Cakrabuwana Putra mahkota Kerajaan Pajajaran membangun Keraton yang kemudian 
+            diserahkan kepada putrinya, Ratu Ayu Pakungwati. Maka Keratonnya dinamai Keraton Pakungwati 
+            (hingga sekarang dikenal dengan sebutan Dalem Agung Pakungwati)</p>
+          </div>
+            <img src="../assets/images/timeline.png" alt="" class="time">
+          
+         </div>
+         <div class="info3">
+            <img src="../assets/images/timeline1.png" alt="" class="time1">
+            <div class="info3-text">
+              <p class="jarak">Pada abad XVI</p> 
+              <p class="jarak1">  <br>Sunan Gunung Jati wafat. Kemudian Pangeran Emas Mochammad Arifin, cicit dari Sunan Gunung Jati,
+                 bertahta menggantikannya. Kemudian pada tahun Candra Sangkala Tunggal Tata Gunaning Wong atau 
+                 1451 Saka yaitu tahun 1529 Masehi beliau mendirikan Keraton baru sebelah barat daya Dalem Agung Pakungwati.
+                 Dan beliaupun bergelar Panembahan Pakungwati I.
+              </p>
+            </div>
+            <img src="../assets/images/k1.png" alt="" class="jarak2">
+         </div>
+         <div class="info4">
+          <img src="../assets/images/m1.png" alt="" class="distance">
+          <div class="info4-text">
+            <p class="jarak3">Pada -+ 1549 M </p>
+            <p><br>Masjid Agung Sang Cipta Rasa kebakaran. Ratu Ayu Pakungwati yang sudah tua itu turut memadamkan api.
+               Api dapat dipadamkan, namun Ratu Ayu Pakungwati kemudian wafat. Semenjak itu, nama atau sebutan Pakungwati
+                dimuliakan dan diabadikan oleh nasab Sunan Gunung Jati.</p>
+                
+          </div><img src="../assets/images/timeline2.png" alt="">
+         </div>
+        <div class="info5">
+          <img src="../assets/images/timeline3.png" alt="" class="time2">
+          <div class="">
+            <p class="jarak4">Pada -+ 1679 M</p>
+           <p><br>
+            Didirikan Keraton Kanoman oleh Sultan Anom I (Sultan Badridin). Maka semenjak itu, Keraton Pakungwati disebut
+               Keraton Kasepuhan hingga sekarang dan sultannya bergelar Sultan Sepuh. Kasepuhan artinya tempat sepuh atau tua. 
+               Jadi, antara Kasepuhan dan Kanoman itu awalnya yang tua dan yang muda (kakak beradik). Lokasi bangunan Keraton 
+               Kasepuhan membujur dari utara ke Selatan atau menghadap ke utara, karena keraton-keraton di Jawa semuanya menghadap 
+               ke utara, artinya menghadap magnet dunia, artinya falsafahnya sang raja mengharapkan kekuatan.</p>
+               
+          </div><img src="../assets/images/n1.png" alt="" class="foto">
+         </div>
+        </div>
+        <div class="footer">
+       
+         <div class="logo">
+            <img src="../assets/images/logo_keraton.png" alt="">
+            <p>KERATON <br>KASEPUHAN <br>CIREBON</p>
+         </div>
+         <div class="footer-col">
+            <div class="footer-col-1">
+               <h3>Quick Links</h3>
+               <ul>
+               <li><a href="#">Beranda</a></li>
+               <li><a href="#">Sejarah</a></li>
+               <li><a href="#">Booking</a></li>
+               <li><a href="#">Objek Wisata</a></li>
+            </ul>
+            </div>
+            <div class="footer-col-2">
+               <h3>Socials</h3>
+               <ul>
+                  <li><a href="#">Whatsapp</a></li>
+                  <li><a href="#">Facebook</a></li>
+                  <li><a href="#">Instagram</a></li>
+                  <li><a href="#">Threads</a></li>
+               </ul>
+            </div>
+            <div class="footer-col-3">
+               <h3>Company</h3>
+               <ul>
+                  <li><a href="#">About us</a></li>
+                  <li><a href="#">Partners</a></li>
+                  <li><a href="#">Contact</a></li>
+               </ul>
+            </div>
+            <div class="footer-col-4">
+               <h3>Subscribes your email for updates!</h3>
+               <button class="button">Enter your email</button>
+               
+
+            </div>
+         </div>
+         <div class="adress">
+            Jalan Kasepuhan 43
+            Cirebon, Jawa Barat
+            45114 
+         </div>
+         <div class="lower">
+            <div class="inlower">
+            @2024 Keraton Kasepuhan Cirebon
+            </div>
+            <div class="susun">
+            <p>In collaboration</p>
+
+            <div class="collab">
+                <img src="../assets/images/logo_keraton.png" alt="" class="foto1">
+               <img src="../assets/images/1 931.svg" alt="" class="foto2">
+               <img src="../assets/images/telkom.svg" alt="" class="foto3">
+               <img src="../assets/images/bjb.svg" alt="" class="foto4">
+               
+            </div>
+         </div>
+         </div>
+        </div>
+      </div>
+    </div>
+  
+
+   
+  
+        
+          
+        
+  
+  
+    
+  
+  
+  
+  
+  
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        uppertext: 'Warisan Leluhur Cirebon',
+        uppertext2: 'Mengenal Lebih Dekat dengan Sejarah Keraton',
+      
+    
+      };
+    }
+  };
+  
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  
+   
+  }
+  @media screen and (max-width: 1000px){ 
+     .navbar-right {
+      flex-direction: row;
+     }
+     .info1 {
+      flex-direction: column;
+      text-align: center;
+      gap:5rem;
+     }
+     .upper {
+      font-size: 100px;
+     }
+     .footer-col {
+      flex-direction: column;
+     }
+     .footer-col-2 , .footer-col-3 {
+      padding-left: 28px;
+     }
+     .footer-col-4 {
+     padding: 0 0 0 0px;
+     text-align: center;
+     width: auto;
+     }
+   
+  }
+  .navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0px 10px 0px;
+  box-shadow: 0 20px 8px rgba(0, 0, 0, 0.1);
+  position: relative;
+  background-color: white;
+  width: 100%;
+  font-family: 'Raleway', sans-serif;
+  
+   
+  }
+  .navbar-right {
+  
+   margin-left: 21.5em;
+   width: 100%;
+  }
+  .navbar-left {
+    display: flex;
+    align-items: center;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    width: 217px;
+    height: 84px;
+  
+  }
+  
+  .app-icon {
+  position: absolute;
+  width: 68px;
+  height: 70.62px;
+  left: 88px;
+  top: 16.69px;
+  
+  }
+  
+  .app-name {
+   
+  position: absolute;
+  width: 139px;
+  height: 84px;
+  left: 166px;
+  top: 10px;
+  color: black;
+  font-size: 20px;
+  }
+  
+  nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+  
+  nav ul li {
+    margin-right: 30px;
+  }
+  nav ul li a,
+  nav ul li button {
+    display: block;
+    color: #000000;
+    background-color: transparent;
+    border: none;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 5px 25px;
+    font-size: 16px;
+    text-decoration: none; 
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  nav ul li a:hover,
+  nav ul li button:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  .button {
+   width:431px;
+   height: 58px;
+   border-radius: 10px;
+   background-color: #123B32;
+   color: #FFFFFF;
+   text-align: left;
+   padding-left: 22px;
+   font-size: 20px;
+   font-weight: 800;
+   margin-top: 22px;
+   line-height: 28px;
+   font-family: "Raleway";
+
+  }
+  .button1 {
+   font-family:'Raleway';
+   font-weight: 700px; 
+   border-radius: 5px; 
+   width: 162px; 
+   height: 34px;
+   background-color: #123B32; 
+   color: #ffffff;
+  }
+  .textAll {
+    font-family: "Raleway", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    color: #212121;
+  
+  
+  
+    
+  }
+
+  body  {
+    margin: 0;
+    padding: 0;
+    width:100%;
+    overflow-x: hidden;
+
+   
+  }
+  .fotoinfo1 {
+    width:483px;
+    height: 445px;
+  }
+  .fotobkg {
+    width:100%;
+  }
+  .upper{
+    position: absolute;
+    margin-top:-31%;
+    margin-left:50%;
+    transform: translate(-50%, -50%);
+    color: #FAE084;
+    font-size: 24px;
+  
+  }
+  .upper-2 {
+      position:absolute;
+      margin-top: -23.5%;
+      margin-left:50%;
+      transform: translate(-50%, -50%);
+      color: #FFF6E0;
+      font-size: 64px;
+      font-weight:400;
+      width: 60%;
+      text-align: center;
+  }
+  
+  .info {
+    
+    margin-left: 108px;
+    margin-right: 108px;
+    padding-top: 77px;
+   
+    
+  }
+  .info1 {
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    width:100%;
+  }
+  .info1 p {
+    margin:0;
+    font-size: 30px;
+    margin-right: 59px;
+    font-weight: 400;
+    color: #212121;
+    line-height: 40px;
+    width:522px;
+  }
+  .info1 img {
+    height: 357;
+    width: 483;
+    border-radius: 10px;
+    margin-right: -15px;
+  }
+  .info2 {
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    margin-top: 157px;
+    width:1064px;
+    margin-left: 30px;
+  
+    
+  }
+  
+  .between {
+   
+   padding-top: 15px;
+  }
+  .info2 p {
+    font-size: 30px;
+    font-weight: 400;
+    color:#212121;
+    text-align: right ;
+    width: 500px;
+    margin-bottom: 100px;
+    margin-top:100px;
+    line-height: 40px;
+    
+    
+  }
+  
+  .info2-text {
+    display: flex;
+    flex-direction: row;
+   
+  }
+  .info2 img {
+    height: auto;
+    border-radius: 10px;
+    height: 426px;
+  }
+  .time {
+    position: relative;
+    margin-left: 24px;
+    padding-bottom: 40px;
+    margin-right: 50px;
+  }
+  .time1 {
+    margin-left:-10px;margin-right: 5px;width:40px;    
+  }
+  
+  .info3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 114px;
+  }
+ 
+  .info3-text {
+    font-size: 30px;
+    font-weight: 400;
+    color:#212121;
+    margin-right: 22px;
+    width: 500px;
+    margin-top:48px;
+    line-height: 40px;
+  }
+  .jarak {
+    margin-left: 29px;
+   
+  }
+  .jarak1{
+    margin-bottom: 57px;
+    
+    width:509px;
+  }
+  .jarak2 {
+    padding-left: 90px;
+  
+  }
+  .jarak4 {
+    padding-top: 50px;
+    margin-left: 29px;
+  }
+  .jarak3{
+   margin-right: 29px;
+   margin-top:10px;
+  }
+  
+  
+  
+  .info4 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 67px;
+  }
+  .info4 p {
+    font-size: 30px;
+    font-weight: 400;
+    color:#212121;
+    text-align: right ;
+    width: 542px;
+    line-height: 40px;
+  }
+  .distance {
+    margin-right: 100px;
+  }
+  .info4-text {
+    display: flex;
+    flex-direction: column;
+    
+    align-items: center;
+    margin-right: 1px;
+  }
+  
+  .info5 {
+    margin-top: 158px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 30px;
+    margin-right: 108px;
+    
+    
+  }
+
+  .info5 p {
+    font-size: 30px;
+    font-weight: 400;
+    color:#212121;
+    text-align: left ;
+    width: 500px;
+    display: flex;
+    line-height: 40px;
+   }
+  .info5 img {
+      margin-left: 50px;
+  }
+
+  .foto {
+    width:483px;
+    height: 294px;
+  }
+  .foto1 {
+   width:68px;
+   height: 71px;
+  }
+  .foto2 {
+   width:69px;
+   height: 74px;
+  }
+  .foto3 {
+   width:53px;
+   height: 71px;
+  }
+  .foto4 {
+   width : 100px;
+   height: 49px;
+  }
+  .footer {
+   width: 100%;
+   height: auto;
+   margin-top: 278px;
+   position: relative;
+ 
+   
+ } 
+ a {
+   text-decoration: none;
+   color: #212121;
+ }
+ ul {
+   list-style-type: none;
+ }
+ .footer-col {
+   display: flex;
+ }
+ .footer-col h3 {
+   font-size: 30px;
+   font-weight: 40px;
+   display: inline-block;
+   margin-bottom: 11px;
+   margin-top: 76px;
+ }
+.footer-col-1 {
+   
+   margin-left: 88px;
+   
+}
+
+ 
+ 
+  .logo {
+    font-size: 20px;
+    font-weight: 400;
+    display: flex;
+    flex-direction: row;
+    margin-left: 88px;
+   
+    
+  }
+  
+  .logo img {
+   width: 70px;
+   margin-right: 9px;
+  
+  }
+
+  .footer-col-2 {
+   margin-left: 60px;
+  }
+  .footer-col-3 {
+   margin-left: 60px;
+  }
+  .footer-col-4 {
+   margin-left: 139px;
+   width: 434px;
+  }
+  .footer-col li {
+      color: #212121;
+  }
+  .footer-col li:not(:last-child) {
+   margin-bottom: 11px;
+  }
+  .adress {
+   width:184px;
+   height: 84px;
+   font-size: 20px;
+   font-weight: 400;
+   margin-top:62px;
+   margin-left: 88px;
+  }
+  .lower {
+    
+    height: 142px;
+    margin-left: 88px;
+    display: flex;
+  }
+  .inlower {
+   padding-top:118px;
+   position: absolute;
+   
+   
+  }
+  .collab {
+   display: flex;
+   flex-direction: row;
+   gap:46px;
+   
+  }
+  .susun {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   gap:46px;
+   padding-left: 645px;
+  }
+  
+  </style>
+  
