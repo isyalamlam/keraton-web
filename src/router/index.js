033b5079ca7db84bb24ba39142eb536627d1b00e
Alipa Gian Rafid <taiee8288@gmail.com>
2024-03-06 07:40:50 +0700

    fix error

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
+import SejarahKeraton from '../views/SejarahKeraton.vue'
+import HistoryTransaksi from '../views/HistoryTransaksi.vue'
+import AreaKeraton from '../views/AreaKeraton.vue'
+import Museum from '../views/Museum.vue'
+import CheckoutKeraton from '../views/CheckoutKeraton.vue'
+import PilihanPaket from '../views/PilihanPaket.vue'
+import CheckOut from '../views/CheckOut.vue'
+import BeranDa from '../views/BeranDa.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+    {
+
+      path: '/sejarahkeraton',
+      name: 'sejarahkeraton',
+      component: SejarahKeraton
+    }
+    ,{
+      path: '/areakeraton',
+      name: 'areakeraton',
+      component: AreaKeraton
+    },
+    {
+      path: '/museum',
+      name: 'museum',
+      component: Museum
+    },
+    {
+ 
+      path: '/checkoutkeraton',
+      name: 'checkoutkeraton',
+      component: CheckoutKeraton
+    },
+    {
+      path: '/pilihanpaket',
+      name: 'pilihanpaket',
+      component: PilihanPaket
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: CheckOut
+    },
+    {
+      path: '/beranda',
+      name: 'beranda',
+      component: BeranDa
+    },
+    {
+      path: '/historytransaksi',
+      name: 'historytransaksi',
+      component: HistoryTransaksi
+
+    },
+  ]
+})
+
+export default router
