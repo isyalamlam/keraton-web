369aa1d9a799e559d38ef32664ebf89238015a0b
Irfan Nurfirmansyah <irfannurfirmansyah1151@gmail.com>
2024-03-07 14:58:58 +0700

    IRFAN EMUACH

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,14 @@ import AreaKeraton from '../views/AreaKeraton.vue'
 import Museum from '../views/Museum.vue'
 import CheckoutKeraton from '../views/CheckoutKeraton.vue'
 import Checkout from '../views/CheckOut.vue'
+<<<<<<< Updated upstream
 import PilihanPaket from '../views/PilihanPaket.vue'
 import CheckOut from '../views/CheckOut.vue'
 import BeranDa from '../views/BeranDa.vue'
 
+=======
+import EventGratis from '../views/EventGratis.vue'
+>>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -54,6 +58,7 @@ const router = createRouter({
     {
       path: '/checkout',
       name: 'checkout',
+<<<<<<< Updated upstream
       component: CheckOut
     },
     {
@@ -67,6 +72,15 @@ const router = createRouter({
       component: HistoryTransaksi
 
     },
+=======
+      component: Checkout
+    },
+    {
+      path: '/eventgratis',
+      name: 'eventgratis',
+      component: EventGratis
+    }
+>>>>>>> Stashed changes
   ]
 })
 
